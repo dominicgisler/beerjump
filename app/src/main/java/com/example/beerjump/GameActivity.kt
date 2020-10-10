019d116ffa25fb1877ac61ade3c1bb24fb90a034
Dominic Gisler <domgisler@gmail.com>
2020-10-11 01:06:35 +0200

    added some game logic

--- app/src/main/java/com/example/beerjump/GameActivity.kt
+++ app/src/main/java/com/example/beerjump/GameActivity.kt
@@ -1,11 +1,13 @@
 package com.example.beerjump
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.util.DisplayMetrics
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game.*
 import kotlin.random.Random
@@ -20,30 +22,60 @@ class GameActivity : AppCompatActivity() {
         val width = displayMetrics.widthPixels
         val height = displayMetrics.heightPixels
 
+        val bars = ArrayList<ImageView>()
         for (i in 0..20) {
             val bar = ImageView(this)
             gameView.addView(bar)
-            bar.layoutParams.width = 80
+            bar.layoutParams.width = 120
             bar.layoutParams.height = 20
-            bar.x = Random.nextFloat() * (width - 80)
+            bar.x = Random.nextFloat() * (width - 120)
             bar.y = Random.nextFloat() * (height - 20)
             bar.setBackgroundColor(Color.GRAY)
+            bars.add(bar)
         }
 
         playerView.layoutParams.width = 70
         playerView.layoutParams.height = 80
 
-        var dirX = 1;
-        var dirY = 1;
+        val act = this
+        var dirX = 0
+        var dirY = 10
         val handler = Handler()
         handler.post(object : Runnable {
             override fun run() {
-                dirX = if (playerView.x <= 0) 1 else if (playerView.x >= (width - 70)) -1 else dirX;
-                dirY = if (playerView.y <= 0) 1 else if (playerView.y >= (height - 80)) -1 else dirY;
-                playerView.x += 10*dirX
-                playerView.y += 10*dirY
-                handler.postDelayed(this, 0)
+                dirY = if (playerView.y <= 0) 10 else dirY
+
+                if (playerView.x < -70) {
+                    playerView.x = width * 1f
+                } else if (playerView.x > width) {
+                    playerView.x = -70f
+                }
+
+                // detect bar contact
+                for (bar in bars) {
+                    if ((playerView.x + 35) in bar.x..(bar.x + 120) && (playerView.y + 80) in bar.y..(bar.y + 20)) {
+                        dirY = -10
+                    }
+                }
+
+                if (playerView.y > height) {
+                    startActivity(Intent(act, MenuActivity::class.java))
+                    finish()
+                } else {
+                    playerView.x += dirX
+                    playerView.y += dirY
+                    handler.postDelayed(this, 0)
+                }
             }
+        })
+
+        directionSlider.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                dirX = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
         })
     }
 }
