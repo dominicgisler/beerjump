2c95750bcf2120f7c46a9c8aa64b5b77aaa5a27d
Nils <nils.mathys@bluewin.ch>
2020-10-13 12:40:41 +0200

    change startpoint, and adding starting bar

--- app/src/main/java/app/beerjump/activity/GameActivity.kt
+++ app/src/main/java/app/beerjump/activity/GameActivity.kt
@@ -14,13 +14,13 @@ import kotlinx.android.synthetic.main.activity_game.*
 import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
-    val barWIth = 240
+    val barWidth = 240
     val barHeight = 40
     val playerWidth = 140
     val playerHeight = 160
     val numberOfBars = 8
     val speedUp = 30
-    val startSpeed = 10
+    val startSpeed = -30
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -31,12 +31,22 @@ class GameActivity : AppCompatActivity() {
         val height = displayMetrics.heightPixels
 
         val bars = ArrayList<ImageView>()
+        val startBar = ImageView(this)
+        gameView.addView(startBar)
+        startBar.layoutParams.width = barWidth
+        startBar.layoutParams.height = barHeight
+        startBar.x = (width / 2 - barWidth / 2).toFloat()
+        startBar.y = (height -220).toFloat()
+        startBar.setBackgroundColor(Color.argb(
+            1f, 0f, 0.8f, 0f
+        ))
+        bars.add(startBar)
         for (i in 0..numberOfBars) {
             val bar = ImageView(this)
             gameView.addView(bar)
-            bar.layoutParams.width = barWIth
+            bar.layoutParams.width = barWidth
             bar.layoutParams.height = barHeight
-            bar.x = Random.nextFloat() * (width - barWIth)
+            bar.x = Random.nextFloat() * (width - barWidth)
             bar.y = Random.nextFloat() * (height - barHeight)
             bar.setBackgroundColor(Color.GRAY)
             bars.add(bar)
@@ -44,6 +54,8 @@ class GameActivity : AppCompatActivity() {
 
         playerView.layoutParams.width = playerWidth
         playerView.layoutParams.height = playerHeight
+        playerView.x = (width / 2 - playerWidth / 2).toFloat()
+        playerView.y = (height-playerHeight).toFloat()
 
         val act = this
         var dirX = 0
@@ -62,7 +74,7 @@ class GameActivity : AppCompatActivity() {
                 // detect bar contact if going down
                 if (dirY > 0) {
                     for (bar in bars) {
-                        if ((playerView.x + playerWidth / 2) in bar.x..(bar.x + barWIth) && (playerView.y + playerHeight) in bar.y..(bar.y + barHeight)) {
+                        if ((playerView.x + playerWidth / 2) in bar.x..(bar.x + barWidth) && (playerView.y + playerHeight) in bar.y..(bar.y + barHeight)) {
                             dirY = -speedUp
                         }
                     }
