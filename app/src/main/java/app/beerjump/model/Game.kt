2916ad0209f5b9b248656dc5d0bbdc1cd13dbcfe
Dominic Gisler <domgisler@gmail.com>
2020-10-18 01:04:58 +0200

    added height logic

--- app/src/main/java/app/beerjump/model/Game.kt
+++ app/src/main/java/app/beerjump/model/Game.kt
@@ -4,29 +4,40 @@ import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import kotlin.random.Random
 
-
 class Game(val gameView: ViewGroup, val playerDrawable: Drawable?) {
-    val numberOfBars = 8
     val speedUp = 30
+    val sectionHeight = 250
+    val sectionPadding = 30
+    var height = 0
 
     val player: Player = Player()
     val bars: ArrayList<Bar> = ArrayList()
 
     fun generate() {
-        val startBar = Bar()
-        startBar.posX = gameView.width / 2 - startBar.width / 2
-        startBar.posY = gameView.height -220
-        bars.add(startBar)
-        for (i in 0..numberOfBars) {
-            val bar = Bar()
-            bar.posX = (Random.nextFloat() * (gameView.width - bar.width)).toInt()
-            bar.posY = (Random.nextFloat() * (gameView.height - bar.height)).toInt()
-            bars.add(bar)
+        // TODO: only generate visible sections
+        for (i in 0..100) {
+            addBarSection(i * sectionHeight)
         }
         player.posX = gameView.width / 2 - player.width / 2
-        player.posY = gameView.height-player.height
+        player.posY = 0
+    }
+
+    private fun addBarSection(startY: Int) {
+        val numBars = (1..3).random()
+        val secWidth = gameView.width / numBars
+        for (j in 0..numBars) {
+            val bar = Bar()
+            val minX = secWidth * j + sectionPadding
+            val maxX = secWidth * (j + 1) - bar.width - sectionPadding
+            val minY = startY + sectionPadding
+            val maxY = startY + sectionHeight - sectionPadding
+            bar.posX = ((Random.nextFloat() * (maxX - minX)) + minX).toInt()
+            bar.posY = ((Random.nextFloat() * (maxY - minY)) + minY).toInt()
+            bars.add(bar)
+        }
     }
 
     fun step(): Boolean {
@@ -41,17 +52,21 @@ class Game(val gameView: ViewGroup, val playerDrawable: Drawable?) {
         // detect bar contact if going down
         if (player.speed < 0) {
             for (bar in bars) {
-                if ((player.posX + player.width / 2) in bar.posX..(bar.posX + bar.width) && (player.posY + player.height) in bar.posY..(bar.posY + bar.height)) {
+                if ((player.posX + player.width / 2) in bar.posX..(bar.posX + bar.width) && (player.posY) in bar.posY..(bar.posY + bar.height)) {
                     player.speed = speedUp
                 }
             }
         }
 
-        if (player.posY > gameView.height) {
+        if (player.posY > (gameView.height / 2 + height) && player.speed > 0) {
+            height += player.speed
+        }
+
+        if (player.posY - height + sectionHeight < 0) {
             return false
         } else {
             player.posX += player.direction
-            player.posY += -player.speed
+            player.posY += player.speed
             return true
         }
     }
@@ -60,12 +75,16 @@ class Game(val gameView: ViewGroup, val playerDrawable: Drawable?) {
         gameView.removeAllViews()
 
         for (bar in bars) {
+            val posY = (gameView.height - bar.posY - bar.height).toFloat() + height
+            if (posY < -sectionHeight || posY > gameView.height + sectionHeight) {
+                continue
+            }
             val viewBar = ImageView(gameView.context)
             gameView.addView(viewBar)
             viewBar.layoutParams.width = bar.width
             viewBar.layoutParams.height = bar.height
             viewBar.x = bar.posX.toFloat()
-            viewBar.y = bar.posY.toFloat()
+            viewBar.y = posY
             viewBar.setBackgroundColor(Color.GRAY)
         }
 
@@ -76,7 +95,20 @@ class Game(val gameView: ViewGroup, val playerDrawable: Drawable?) {
         playerView.layoutParams.width = player.width
         playerView.layoutParams.height = player.height
         playerView.x = player.posX.toFloat()
-        playerView.y = player.posY.toFloat()
+        playerView.y = (gameView.height - player.posY - player.height).toFloat() + height
+
+        val heightLabel = TextView(gameView.context)
+        heightLabel.x = sectionPadding.toFloat()
+        heightLabel.y = sectionPadding.toFloat()
+        heightLabel.textSize = 18f
+        heightLabel.text = "Aktuelle Höhe"
+        val heightValue = TextView(gameView.context)
+        heightValue.x = sectionPadding.toFloat()
+        heightValue.y = heightLabel.y + 40
+        heightValue.textSize = 32f
+        heightValue.text = height.toString()
+        gameView.addView(heightLabel)
+        gameView.addView(heightValue)
     }
 
     fun setDirection(direction: Int) {
