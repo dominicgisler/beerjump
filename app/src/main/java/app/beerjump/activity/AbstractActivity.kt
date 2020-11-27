b32e073cd0a3448e032385482ffb966fba07773f
Dominic Gisler <domgisler@gmail.com>
2020-11-27 17:25:26 +0100

    set decor view on resume, hide action bar after reopening app

--- app/src/main/java/app/beerjump/activity/AbstractActivity.kt
+++ app/src/main/java/app/beerjump/activity/AbstractActivity.kt
@@ -13,13 +13,22 @@ abstract class AbstractActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        setDecorView()
+        config = Config(this.getSharedPreferences("config", Context.MODE_PRIVATE))
+        config.highscoreList.scores.sortByDescending { it.score }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setDecorView()
+    }
+
+    fun setDecorView() {
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
-        config = Config(this.getSharedPreferences("config", Context.MODE_PRIVATE))
-        config.highscoreList.scores.sortByDescending { it.score }
     }
 }
