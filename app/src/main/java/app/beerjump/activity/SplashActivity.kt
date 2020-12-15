ce41450bdd25012e12bbd59ed197d8ebd18d6ec9
Dominic Gisler <domgisler@gmail.com>
2020-12-15 11:23:45 +0100

    added api usage to register device

--- app/src/main/java/app/beerjump/activity/SplashActivity.kt
+++ app/src/main/java/app/beerjump/activity/SplashActivity.kt
@@ -4,13 +4,31 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import app.beerjump.R
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 class SplashActivity : AbstractActivity() {
     private val SPLASH_TIME_OUT:Long = 3000 // 3 sec
+    private val DEVICE_URL = "https://api.beerjump.app/device/%s"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val queue = Volley.newRequestQueue(baseContext)
+
+        val jsonObject = JSONObject()
+        try {
+            jsonObject.put("sdk", android.os.Build.VERSION.SDK_INT)
+            jsonObject.put("brand", android.os.Build.BRAND)
+            jsonObject.put("model", android.os.Build.MODEL)
+        } catch (e: JSONException) {}
+
+        val req = JsonObjectRequest(Request.Method.PUT, String.format(DEVICE_URL, config.uuid), jsonObject, {}, {})
+        queue.add(req)
 
         Handler().postDelayed({
             startActivity(Intent(this, MenuActivity::class.java))
--- app/src/main/java/app/beerjump/model/Config.kt
+++ app/src/main/java/app/beerjump/model/Config.kt
@@ -3,10 +3,12 @@ package app.beerjump.model
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import java.lang.Exception
+import java.util.*
 
 class Config(val sharedPref: SharedPreferences) {
     var highscoreList: HighscoreList = HighscoreList()
     var inputMethod = "touch"
+    var uuid = ""
 
     init {
         val data = sharedPref.getString("highscore", "{\"last_user\":\"\",\"scores\":[]}")
@@ -18,6 +20,8 @@ class Config(val sharedPref: SharedPreferences) {
             }
         }
         inputMethod = sharedPref.getString("input", "touch").toString()
+        uuid = sharedPref.getString("uuid", UUID.randomUUID().toString()).toString()
+        save()
     }
 
     fun save() {
@@ -25,6 +29,7 @@ class Config(val sharedPref: SharedPreferences) {
             val json = Gson().toJson(highscoreList)
             putString("highscore", json)
             putString("input", inputMethod)
+            putString("uuid", uuid)
             apply()
         }
     }
